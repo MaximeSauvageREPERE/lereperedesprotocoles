4e0e32461450d37f87b428afe90f5cb90f4388ed
MaximeSauvageREPERE <maxime-sauvage@live.fr>
2026-01-21 19:29:21 +0400

    Fix : enlever le champs mot de passe de l'edit de utilisateur

--- src/Form/UtilisateurType.php
+++ src/Form/UtilisateurType.php
@@ -6,7 +6,6 @@ use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -27,11 +26,6 @@ class UtilisateurType extends AbstractType
             ->add('email', EmailType::class, [
                 'label' => 'Email',
                 'attr' => ['class' => 'form-control'],
-            ])
-            ->add('password', PasswordType::class, [
-                'label' => 'Mot de passe',
-                'attr' => ['class' => 'form-control'],
-                'required' => false,
             ])
             ->add('roles', ChoiceType::class, [
                 'label' => 'Rôles',
