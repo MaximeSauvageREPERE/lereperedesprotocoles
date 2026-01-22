e19fbae54de8f64ef12a98c73e234f17b9846c4a
Maxime SAUVAGE <msauvage.cda@FORMATION.LOCAL>
2026-01-22 16:01:53 +0400

    Fix : suppression route show domaine et suppression du bonton associé

--- src/Controller/DomaineController.php
+++ src/Controller/DomaineController.php
@@ -44,14 +44,6 @@ final class DomaineController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_domaine_show', methods: ['GET'])]
-    public function show(Domaine $domaine): Response
-    {
-        return $this->render('domaine/show.html.twig', [
-            'domaine' => $domaine,
-        ]);
-    }
-
     #[Route('/{id}/edit', name: 'app_domaine_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Domaine $domaine, EntityManagerInterface $entityManager): Response
     {
